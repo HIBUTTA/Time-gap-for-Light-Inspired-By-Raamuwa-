db262858ac7eee38c1462e3f87fc9daa4089937f
HIBUTTA <60877414+HIBUTTA@users.noreply.github.com>
2022-07-16 16:18:16 +0530

    Just clear the code

--- time to light gaps.py
+++ time to light gaps.py
@@ -1,21 +1,14 @@
 # Written BY Dimuthu Praboda(Hibutta)..........
-#කතෘ හිමිකම් සුරකින්න.
+# කතෘ හිමිකම් සුරකින්න.
 
 import math
-#x=float(input("Number"))
-#print(math.sqrt(x))
-
-
 
 print("____________________Time to light gaps_________________________\n")
 
 t0 = float(input("t0 :- "))
 v = float(input("v :- "))
-#c = float(input("c :- "))
 c = 299792458
-c=float(c)
-#v=c*0.9
-#v=float(v)
+c = float(c)
 
 
 t = t0/math.sqrt(1-((v*v)/(c*c)))
@@ -24,11 +17,5 @@ print(t)
 print("\n\n________________end_________________\n\n")
 
 
-#print(v*v)
-#print(c*c)
-#print((v*v)/(c*c))
-
-
-
 # Written BY Dimuthu Praboda(Hibutta)..........
-#කතෘ හිමිකම් සුරකින්න.
+# කතෘ හිමිකම් සුරකින්න.
